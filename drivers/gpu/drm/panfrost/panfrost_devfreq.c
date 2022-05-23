41d4a851505cc2625cbc7928c1d19a3a302ae278
Alexey Sheplyakov <asheplyakov@basealt.ru>
2022-05-23 19:34:31 +0400

    drm/panfrost: disable devfreq on Baikal-M
    Enabling GPU frequency scaling on Baikal-M cases GPU MMU lockup:
    
      [   38.108633] panfrost 2a200000.gpu: AS_ACTIVE bit stuck
    
    Since GPU and CPU share the memory this locks up the whole system.
    Therefore disable devfreq on Baikal-M.
    
    Signed-off-by: Alexey Sheplyakov <asheplyakov@basealt.ru>
    X-DONTUPSTREAM
    X-feature-Baikal-M

--- drivers/gpu/drm/panfrost/panfrost_devfreq.c
+++ drivers/gpu/drm/panfrost/panfrost_devfreq.c
@@ -100,6 +100,11 @@ int panfrost_devfreq_init(struct panfrost_device *pfdev)
 		DRM_DEV_INFO(dev, "More than 1 supply is not supported yet\n");
 		return 0;
 	}
+	if (of_device_is_compatible(of_root, "baikal,baikal-m") ||
+			of_device_is_compatible(of_root, "baikal,bm1000")) {
+		dev_info(pfdev->dev, "disabling GPU devfreq on BE-M1000\n");
+		return 0;
+	}
 
 	ret = devm_pm_opp_set_regulators(dev, pfdev->comp->supply_names);
 	if (ret) {
