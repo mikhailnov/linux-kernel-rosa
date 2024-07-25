d7db446577e2fcc2d0d71470c69923059c37a5e6
Daniil Gnusarev <gnusarevda@basealt.ru>
2024-07-25 15:50:34 +0400

    uart: add support for UART Baikal BE-M1000
    Add compatible string for DW8250 serial port driver of Baikal BE-M1000
    
    Signed-off-by: Daniil Gnusarev <gnusarevda@basealt.ru>
    Co-developed-by: Baikal Electronics <info@baikalelectronics.ru>

--- drivers/tty/serial/8250/8250_dw.c
+++ drivers/tty/serial/8250/8250_dw.c
@@ -755,8 +755,14 @@ static const struct dw8250_platform_data dw8250_skip_set_rate_data = {
 	.quirks = DW_UART_QUIRK_SKIP_SET_RATE,
 };
 
+static const struct dw8250_platform_data dw8250_baikal_bm1000_data = {
+	.usr_reg = DW_UART_USR,
+	.quirks = DW_UART_QUIRK_SKIP_SET_RATE,
+};
+
 static const struct of_device_id dw8250_of_match[] = {
 	{ .compatible = "snps,dw-apb-uart", .data = &dw8250_dw_apb },
+	{ .compatible = "baikal,bm1000-uart", .data = &dw8250_baikal_bm1000_data },
 	{ .compatible = "cavium,octeon-3860-uart", .data = &dw8250_octeon_3860_data },
 	{ .compatible = "marvell,armada-38x-uart", .data = &dw8250_armada_38x_data },
 	{ .compatible = "renesas,rzn1-uart", .data = &dw8250_renesas_rzn1_data },
