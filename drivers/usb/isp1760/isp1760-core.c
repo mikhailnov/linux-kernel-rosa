911bd7a077f8c83f7cf6469efdf48e415cefda9c
Mikhail Novosyolov <m.novosyolov@rosa.ru>
2025-07-21 09:18:54 +0300

    usb: isp1760: fix typo leading to out-of-bounds array access
    Found by Svace:
    Value 68 is used in function 'isp1760_field_set' at
    isp1760-core.c:115 to calculate an index for accessing an
    element of array 'isp->udc->fields' of size 40. This may
    lead to a buffer overflow. The out of range index value is
    originated from isp1760-core.h:76. (CWE119, CWE121,
    CWE122, CWE124, CWE125, CWE126)
    
    Previuos attempt to fix this problem was here:
    http://linuxtesting.org/pipermail/lvc-project/2025-April/004680.html
    
    Fixes: 1da9e1c06873 ("usb: isp1760: move to regmap for register access")
    Co-authored-by: DeepSeek
    Signed-off-by: Mikhail Novosyolov <m.novosyolov@rosa.ru>

--- drivers/usb/isp1760/isp1760-core.c
+++ drivers/usb/isp1760/isp1760-core.c
@@ -109,12 +109,10 @@ static int isp1760_init_core(struct isp1760_device *isp)
 
 void isp1760_set_pullup(struct isp1760_device *isp, bool enable)
 {
-	struct isp1760_udc *udc = &isp->udc;
-
 	if (enable)
-		isp1760_field_set(udc->fields, HW_DP_PULLUP);
+		isp1760_field_set(isp->hcd.fields, HW_DP_PULLUP);
 	else
-		isp1760_field_set(udc->fields, HW_DP_PULLUP_CLEAR);
+		isp1760_field_set(isp->hcd.fields, HW_DP_PULLUP_CLEAR);
 }
 
 /*
